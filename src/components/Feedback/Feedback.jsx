8b4e06a5a49977cc2f5c8f445a71df14426dbac6
ruslana <ruslana.lohvyn@gmail.com>
2024-12-16 19:43:42 +0200

    update paddings

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,9 +1,9 @@
-// import React from 'react'
+import style from "./feedback.module.css"
 
 const Feedback = ({ feedbackTypes, totalFeedback, positive }) => {
   return (
       <>
-          <ul>
+      <ul className={style.list}>
             <li>{`Good: ${feedbackTypes.good}`}</li>
             <li>{`Neutral: ${feedbackTypes.neutral}`}</li>
             <li>{`Bad: ${feedbackTypes.bad}`}</li>
